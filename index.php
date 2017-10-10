471f5b01f1a79c1605a2928606a5d59ae00e6533
Ellie Hutchins <32098835+HutchinsEBPCMagenta@users.noreply.github.com>
2017-10-10 13:39:13 +0100

    new php file

---
+++ index.php
@@ -0,0 +1,21 @@
+<HTML>
+
+<BODY>
+
+<?php
+
+$mytext1 = "Good morning!";
+$mytext2 = "Suits you sir!";
+
+echo "The first string is: '";
+echo $mytext1;
+echo "'<br/><br/>";
+echo "The second string is: '";
+echo $mytext2;
+echo "'<br/><br/>";
+
+?>
+
+</BODY>
+
+</HTML>
