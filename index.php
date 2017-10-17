e21499a9590f7a58226d0a820f264284b1ce5361
Ellie Hutchins <32098835+HutchinsEBPCMagenta@users.noreply.github.com>
2017-10-17 13:56:50 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<HTML>
+
+<BODY>
+
+<?php
+
+$myfirst = "Hello world!";
+$firstnum = 4;
+$firstfloat = 24.2;
+$fullname = "Ellie Hutchins"
+$age = "17"
+$dob = "13/07/2000"
+
+echo $myfirst;
+echo "<br>";
+echo $firstnum;
+echo "<br>";
+echo $firstfloat;
+echo "<br>";
+echo $fullname;
+echo "<br>";
+echo $age;
+echo "<br>";
+echo $dob;
+echo "<br>";
+
+?>
+
+</BODY>
+
+</HTML>
+
